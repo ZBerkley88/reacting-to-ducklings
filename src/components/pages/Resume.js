c46a69607d1f004752cc0405aacee07feeea1fe2
Zack <zachary.berkley@gmail.com>
2023-02-11 23:00:01 -0500

    download resume button working

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,15 +1,27 @@
 import React from "react";
 
 export default function Resume() {
+  // Function will execute on click of button
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch("ZMB Resume 2023.pdf").then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "ZMB Resume 2023.pdf";
+        alink.click();
+      });
+    });
+  };
   return (
-    <div className="page">
-      <h1>Resume Page</h1>
-      <a
-        type="button"
-        className="btn btn-secondary btn-lg"
-        href="/public/ZMB Resume 2023.pdf"
-        download
-      >Download Me</a>
-    </div>
+    <>
+      <center>
+        <h3>Click on below button to download my resume</h3>
+        <button onClick={onButtonClick}>Download Resume</button>
+      </center>
+    </>
   );
 }
