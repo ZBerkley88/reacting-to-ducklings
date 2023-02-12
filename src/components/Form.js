d80afbce39d805835dfa03bce6ba85e6d67b6877
Zack <zachary.berkley@gmail.com>
2023-02-12 12:57:11 -0500

    form validation functioning, still requires some styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,45 +1,105 @@
+// prevents rule warning from being thrown for the Regex in line 6
+/* eslint-disable */
+
 import React from 'react';
 
-class Form extends React.Component {
+// source: https://www.educative.io/answers/react-form-validation
+const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+
+const validateForm = errors => {
+  let valid = true;
+  Object.values(errors).forEach(val => val.length > 0 && (valid = false));
+  return valid;
+};
+
+export default class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      email: '',
-      message: ''
+      name: "",
+      email: "",
+      message: "",
+      errors: {
+        name: '',
+        email: '',
+        message: '',
+      }
+    };
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case 'name': 
+        errors.name = 
+          value.length < 1
+            ? 'This field is required!'
+            : '';
+        break;
+      case 'email': 
+        errors.email = 
+          validEmailRegex.test(value)
+            ? ''
+            : 'Email is not valid!';
+        break;
+      case 'message': 
+        errors.message = 
+          value.length < 1
+            ? 'This field is required!'
+            : '';
+        break;
+      default:
+        break;
+    }
+
+    this.setState({errors, [name]: value});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if(validateForm(this.state.errors)) {
+      console.info('Valid Form')
+    }else{
+      console.error('Invalid Form')
     }
   }
+
   render() {
-    return(
-      <div className="form">
-        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message</label>
-            <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+    const {errors} = this.state;
+    return (
+      <div className='wrapper'>
+        <div className='form-wrapper'>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className='name'>
+              <label htmlFor="name">Name</label>
+              <input type='text' name='name' onChange={this.handleChange} noValidate />
+              {errors.name.length > 0 && 
+                <span className='error'>{errors.fullName}</span>}
+            </div>
+            <div className='email'>
+              <label htmlFor="email">Email</label>
+              <input type='email' name='email' onChange={this.handleChange} noValidate />
+              {errors.email.length > 0 && 
+                <span className='error'>{errors.email}</span>}
+            </div>
+            <div className='message'>
+              <label htmlFor="message">Message</label>
+              <input type='message' name='message' onChange={this.handleChange} noValidate />
+              {errors.message.length > 0 && 
+                <span className='error'>{errors.message}</span>}
+            </div>
+            <div className='submit'>
+              <button>Submit</button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
-  onNameChange(event) {
-    this.setState({name: event.target.value})
-  }
-  onEmailChange(event) {
-    this.setState({email: event.target.value})
-  }
-  onMessageChange(event) {
-    this.setState({message: event.target.value})
-  }
-  handleSubmit(event) {
-  }
 }
-export default Form;
