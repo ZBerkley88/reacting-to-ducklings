3045bba7b40b9170d355b3b7dfa291b4118b5e04
Zack <zachary.berkley@gmail.com>
2023-02-13 18:52:42 -0500

    project cards complete

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,29 +5,178 @@ export default function Portfolio() {
     <div className="page">
       <h1 className="page-title">Portfolio</h1>
 
-      <ul className="project-grid">
-        <a href="https://libscrib-app.herokuapp.com/" target="_blank" rel="noreferrer">
-          <li className="project-card">
-            <h3 className="project-title">LibScrib</h3>
-            <div className="project-description">
-              <p>
-                {" "}
-                Allows users to search for information about a desired book.
-                When the user enters in the title of a book, the application
-                searches through the NYTBS Database and finds all pertinent
-                information about the book.
-              </p>
-            </div>
-            <footer>
-              <ul className="skill-list">
-                <li>Handlebars</li>
-                <li>Node</li>
-                <li>MySQL</li>
-              </ul>
-            </footer>
-          </li>
-        </a>
+      {/* LIBSCRIB */}
+      <ul className="container-fluid">
+        <div className="row">
+          <a
+            href="https://libscrib-app.herokuapp.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="col-12 col-md-4"
+          >
+            <li className="project-card">
+              <h3 className="project-title">LibScrib</h3>
+              <div className="project-description">
+                <p>
+                  {" "}
+                  Allows users to search for information about a desired book.
+                  When the user enters in the title of a book, the application
+                  searches through the NYTBS Database and finds all pertinent
+                  information about the book.
+                </p>
+              </div>
+              <footer>
+                <ul className="skill-list">
+                  <li>Handlebars</li>
+                  <li>Node</li>
+                  <li>MySQL</li>
+                </ul>
+              </footer>
+            </li>
+          </a>
+
+          {/* EXPRESS NOTE TAKER */}
+          <a
+            href="https://rocky-lowlands-48543.herokuapp.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="col-12 col-md-4"
+          >
+            <li className="project-card">
+              <h3 className="project-title">Express Note Taker</h3>
+              <div className="project-description">
+                <p>
+                  {" "}
+                  This app uses Express.js to save and retrieve data stored in a
+                  JSON file. Users can view previously stored notes by clicking
+                  on the note title in the sidebar. Users can also delete a note
+                  by clicking on the red trash icon beside it.
+                </p>
+              </div>
+              <footer>
+                <ul className="skill-list">
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript</li>
+                </ul>
+              </footer>
+            </li>
+          </a>
+
+          {/* PLACEHOLDER */}
+          <a
+            href="google.com"
+            target="_blank"
+            rel="noreferrer"
+            className="col-12 col-md-4"
+          >
+            <li className="project-card">
+              <h3 className="project-title">Placeholder</h3>
+              <div className="project-description">
+                <p>
+                  {" "}
+                  Placeholder text
+                </p>
+              </div>
+              <footer>
+                <ul className="skill-list">
+                  <li>skillOne</li>
+                  <li>skillTwo</li>
+                  <li>skillThree</li>
+                </ul>
+              </footer>
+            </li>
+          </a>
+
+
+        </div>
+
+        
       </ul>
+
+      <ul className="container-fluid">
+        <div className="row">
+          {/* PLACEHOLDER */}
+          <a
+            href="google.com"
+            target="_blank"
+            rel="noreferrer"
+            className="col-12 col-md-4"
+          >
+            <li className="project-card">
+              <h3 className="project-title">Placeholder</h3>
+              <div className="project-description">
+                <p>
+                  {" "}
+                  Placeholder text
+                </p>
+              </div>
+              <footer>
+                <ul className="skill-list">
+                  <li>skillOne</li>
+                  <li>skillTwo</li>
+                  <li>skillThree</li>
+                </ul>
+              </footer>
+            </li>
+          </a>
+
+          {/* PLACEHOLDER */}
+          <a
+            href="google.com"
+            target="_blank"
+            rel="noreferrer"
+            className="col-12 col-md-4"
+          >
+            <li className="project-card">
+              <h3 className="project-title">Placeholder</h3>
+              <div className="project-description">
+                <p>
+                  {" "}
+                  Placeholder text
+                </p>
+              </div>
+              <footer>
+                <ul className="skill-list">
+                  <li>skillOne</li>
+                  <li>skillTwo</li>
+                  <li>skillThree</li>
+                </ul>
+              </footer>
+            </li>
+          </a>
+
+          {/* PLACEHOLDER */}
+          <a
+            href="google.com"
+            target="_blank"
+            rel="noreferrer"
+            className="col-12 col-md-4"
+          >
+            <li className="project-card">
+              <h3 className="project-title">Placeholder</h3>
+              <div className="project-description">
+                <p>
+                  {" "}
+                  Placeholder text
+                </p>
+              </div>
+              <footer>
+                <ul className="skill-list">
+                  <li>skillOne</li>
+                  <li>skillTwo</li>
+                  <li>skillThree</li>
+                </ul>
+              </footer>
+            </li>
+          </a>
+
+
+        </div>
+
+        
+      </ul>
+
     </div>
   );
 }
